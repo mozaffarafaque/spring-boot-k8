6bd1183999daba220352d100689e7c827abba536
Mozaffar Afaque <afaque.mozaffar@gmail.com>
2020-10-02 12:04:17 +0530

    Test app

--- app-gateway/src/main/java/com/mozafaq/test/springboot/appgateway/Application.java
+++ app-gateway/src/main/java/com/mozafaq/test/springboot/appgateway/Application.java
@@ -13,7 +13,7 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication application =  new SpringApplication(Application.class);
-        application.setDefaultProperties(Collections.singletonMap("server.port", "8081"));
+        //application.setDefaultProperties(Collections.singletonMap("server.port", "8081"));
         application.run(args);
     }
 }
--- etc/run.sh
+++ etc/run.sh
@@ -2,12 +2,12 @@ DIR=$(dirname $0)
 
 echo "Dir name $DIR"
 
-APP_IMAGE="test-springboot-app:1.0"
+APP_IMAGE="test-springboot-app:1.1"
 
 docker build -f Dockerfile -t ${APP_IMAGE} ..
 
 
-docker container run -it -d -p 8081:8081 --name container-sample-app ${APP_IMAGE}
+docker container run -it -d -p 8081:8080 --name container-sample-app ${APP_IMAGE}
 
 echo "Started...."
 
