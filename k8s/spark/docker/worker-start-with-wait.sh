d684d81d356a4652eef0e49f9726c8e74067fd8d
Mozaffar Afaque <afaque.mozaffar@gmail.com>
2021-01-08 19:25:01 +0530

    Test app

--- k8s/spark/docker/worker-start-with-wait.sh
+++ k8s/spark/docker/worker-start-with-wait.sh
@@ -3,6 +3,6 @@
 set -e
 
 
-/opt/spark/sbin/start-worker.sh spark://spark-master:9011 -c 2 -m 2G -p 9021 --webui-port 9022
+/opt/spark/sbin/start-worker.sh spark://spark-master-server:9011 -c 2 -m 2G -p 9021 --webui-port 9022
 
 sleep 86400
