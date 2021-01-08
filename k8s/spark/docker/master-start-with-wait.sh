9c1358f90f9864e5ac7cba4803f884e2e761bd18
Mozaffar Afaque <afaque.mozaffar@gmail.com>
2021-01-08 23:59:50 +0530

    Test app

--- k8s/spark/docker/master-start-with-wait.sh
+++ k8s/spark/docker/master-start-with-wait.sh
@@ -2,7 +2,8 @@
 
 set -e
 
+IP=$(hostname -i)
 
-/opt/spark/sbin/start-master.sh -p 9011 --webui-port 9012
+/opt/spark/sbin/start-master.sh -p 9011 --webui-port 9012 -h $IP
 
 sleep 86400
--- k8s/spark/docker/worker-start-with-wait.sh
+++ k8s/spark/docker/worker-start-with-wait.sh
@@ -2,7 +2,8 @@
 
 set -e
 
+IP=$(hostname -i)
 
-/opt/spark/sbin/start-worker.sh spark://spark-master-server:9011 -c 2 -m 2G -p 9021 --webui-port 9022
+/opt/spark/sbin/start-worker.sh spark://spark-master-server:9011 -c 2 -m 2G -p 9021 --webui-port 9022 -h $IP
 
 sleep 86400
