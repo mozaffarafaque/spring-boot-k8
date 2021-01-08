79f77d08fce70c879db71722522512c690483bed
Mozaffar Afaque <afaque.mozaffar@gmail.com>
2021-01-08 18:39:20 +0530

    Test app

--- k8s/spark/docker/worker-start-with-wait.sh
+++ k8s/spark/docker/worker-start-with-wait.sh
@@ -3,12 +3,6 @@
 set -e
 
 
-#echo "USERNAME: $USER \n" >> /opt/spark/logs/boot.txt
-
-#echo "USERNAME: $USER \n" >> /opt/spark/logs/boot.txt
-
-
-
 /opt/spark/sbin/start-worker.sh spark://spark-master:9011 -c 2 -m 2G -p 9021 --webui-port 9022
 
-sleep 900
+sleep 86400
